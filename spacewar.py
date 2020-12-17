8fa70f88479a21402022626fe8c2a3a0cb15eee5
sueda3445 <74103028+sueda3445@users.noreply.github.com>
2020-12-17 17:28:05 +0300

    Update spacewar.py

--- spacewar.py
+++ spacewar.py
@@ -2,83 +2,95 @@
 import turtle, random
 import winsound
 
-screen = turtle.Screen()
-secreen.bgcolor('black')
-screen.title('Uzay savaşı')
-screen.bgpic('uzay.gif')
-screen.setup(width=600, height=600)
+pencere = turtle.Screen()
+pencere.bgcolor('black')
+pencere.title('Uzay savaşı')
+pencere.bgpic('uzay.gif')
+pencere.setup(width=800, height=600)
 
 turtle.register_shape('oyuncu.gif')
 turtle.register_shape('dusman.gif')
 turtle.register_shape('ates.gif')
 
-player = turtle.Turtle()
-player.color('blue')
-player.speed(0)
-player.shape('oyuncu.gif')
-player.setheading(90)
-player.penup()
-player.goto(0, -250)
-playerhizi = 20
+oyuncu = turtle.Turtle()
+oyuncu.color('blue')
+oyuncu.speed(0)
+oyuncu.shape('oyuncu.gif')
+oyuncu.setheading(90)
+oyuncu.penup()
+oyuncu.goto(0, -250)
+oyuncuhizi = 20
 
-fire = turtle.Turtle()
-fire .color('yellow')
-fire .speed(0)
-fire .shape('ates.gif')
-fire .setheading(90)
-fire .penup()
-fire .goto(0, -240)
-fire hizi = 20
-fire .hideturtle()
-fire .turtlesize(1, 1)
-fire kontrol = False
+ates = turtle.Turtle()
+ates.color('yellow')
+ates.speed(0)
+ates.shape('ates.gif')
+ates.setheading(90)
+ates.penup()
+ates.goto(0, -240)
+ateshizi = 20
+ates.hideturtle()
+ates.turtlesize(1, 1)
+ateskontrol = False
 
-write = turtle.Turtle()
-write.color('white')
-write.speed(0)
-write.penup()
-write.goto(0, 200)
-write.hideturtle()
+yaz = turtle.Turtle()
+yaz.color('white')
+yaz.speed(0)
+yaz.penup()
+yaz.goto(0, 200)
+yaz.hideturtle()
 
-def firegit():
+
+def atesgit():
     y = ates.ycor()
     y = y + ateshizi
     ates.sety(y)
-def left_move():
-    x = player.xcor()
-    x = x - playerhizi
+
+
+def sola_git():
+    x = oyuncu.xcor()
+    x = x - oyuncuhizi
     if x < -300:
         x = -300
-    player.setx(x)
-def right_move():
-    x = player.xcor()
-    x = x + playerhizi
+    oyuncu.setx(x)
+
+
+def saga_git():
+    x = oyuncu.xcor()
+    x = x + oyuncuhizi
     if x > 300:
         x = 300
-    player.setx(x)
-def up_move():
-    y = player.ycor()
-    y = y + playerhizi
+    oyuncu.setx(x)
+
+
+def yukari_git():
+    y = oyuncu.ycor()
+    y = y + oyuncuhizi
     if y > 270:
         y = 270
-    player.sety(y)
-def down_move():
-    y = player.ycor()
-    y = y - playerhizi
+    oyuncu.sety(y)
+
+
+def asagi_git():
+    y = oyuncu.ycor()
+    y = y - oyuncuhizi
     if y < -270:
         y = -270
-    player.sety(y)
+    oyuncu.sety(y)
+
+
 def ateset():
     global ateskontrol
     winsound.PlaySound('lazer.wav', winsound.SND_ASYNC)
-    x = player.xcor()
-    y = player.ycor() + 20
-    fire.goto(x, y)
-    fire.showturtle()
-    firekontrol = True
+    x = oyuncu.xcor()
+    y = oyuncu.ycor() + 20
+    ates.goto(x, y)
+    ates.showturtle()
+    ateskontrol = True
+
 
 hedefler = []
-for i in range(7):
+for i in range(5):
     hedefler.append(turtle.Turtle())
 for hedef in hedefler:
     hedef.color('red')
@@ -91,16 +103,15 @@ for hedef in hedefler:
     y = random.randint(180, 260)
     hedef.goto(x, y)
 
-
-screen.listen()
-screen.onkey(left_move,'Left')
-screen.onkey(right_move,'Right')
-screen.onkey(up_move,'Up')
-screen.onkey(down_move,'Down')
-screen.onkey(fireet, 'space')
+pencere.listen()
+pencere.onkey(sola_git, 'Left')
+pencere.onkey(saga_git, 'Right')
+pencere.onkey(yukari_git, 'Up')
+pencere.onkey(asagi_git, 'Down')
+pencere.onkey(ateset, 'space')
 
 while True:
-    if firekontrol:
+    if ateskontrol:
         atesgit()
     for hedef in hedefler:
         y = hedef.ycor()
@@ -112,6 +123,7 @@ while True:
             hedefler.pop(hedefler.index(hedef))
             winsound.PlaySound('patlama.wav', winsound.SND_ASYNC)
         if hedef.ycor() < -270 or hedef.distance(oyuncu) < 20:
-            yaz.write('Maalesef, kabettiniz!', align='center', font=('Courier', 24, 'bold'))
+            winsound.PlaySound('oyunbitti.wav', winsound.SND_ASYNC)
+            yaz.write('Maalesef, kaybettiniz!', align='center', font=('Courier', 24, 'bold'))
     if len(hedefler) == 0:
         yaz.write('Tebrikler, kazandınız!', align='center', font=('Courier', 24, 'bold'))
