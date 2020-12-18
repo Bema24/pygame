4191b12d09ce4692891c7c8ecaf3ba781b8fddcf
sueda3445 <74103028+sueda3445@users.noreply.github.com>
2020-12-18 16:39:18 +0300

    Update spacewar.py

--- spacewar.py
+++ spacewar.py
@@ -124,6 +124,6 @@ while True:
             winsound.PlaySound('patlama.wav', winsound.SND_ASYNC)
         if hedef.ycor() < -270 or hedef.distance(oyuncu) < 20:
             winsound.PlaySound('oyunbitti.wav', winsound.SND_ASYNC)
-            yaz.write('Maalesef, kaybettiniz!', align='center', font=('Courier', 24, 'bold'))
+            yaz.write('Sorry, You Lost!', align='center', font=('Courier', 24, 'bold'))
     if len(hedefler) == 0:
-        yaz.write('Tebrikler, kazandınız!', align='center', font=('Courier', 24, 'bold'))
+        yaz.write('Congratulations, You Won!!', align='center', font=('Courier', 24, 'bold'))
